3f198ac35786fb0893d282d98bfb613586fb6a63
Howard Kiprono <howardbet01@gmail.com>
2023-10-23 14:43:59 +0300

    inputed some variables in C

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+int myNum = 5;
+float myFloatNum = 5.99;
+char myLetter = 'D';
+
+printf("%d\n", myNum);
+printf("%f\n", myFloatNum);
+printf("%c\n", myLetter);
+}
